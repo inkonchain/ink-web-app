f3f543b6fadc0b889a331e5aed8d5a6dd33fd155
ink-victor <171172553+ink-victor@users.noreply.github.com>
2025-02-21 18:10:50 -0500

    chore: remove old smart wallet experiment

--- src/app/[locale]/smart-wallet/lib/viem.ts
+++
@@ -1,7 +0,0 @@
-import { createPublicClient, http } from "viem";
-
-import { BUNDLER_URL } from "../utils/constants";
-
-export const publicClient = createPublicClient({
-  transport: http(BUNDLER_URL),
-});
